234c1fe71eada2aabb2e20e26202499e3cc9d60c
guilhermearaujoo <ghma.em1@gmail.com>
2023-01-01 14:14:11 -0300

    Criei restaurants service e um tratador de erros

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,3 +1,19 @@
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { MEAT_API } from "app.api";
+import { Observable } from "rxjs";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+import { Restaurant } from "./restaurant/restaurant.model";
+import { ErrorHandler } from "app/app.error-handler";
+@Injectable()
 export class RestaurantService {
+  constructor(public http : Http) {
+  }
 
+  restaurants() : Observable<Restaurant[]> {
+    return this.http.get(`${MEAT_API}/restaurants`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
+  }
 }
